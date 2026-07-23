a59dc2da27363fac10e6aad20afe09996f4cbc73
Cihan Tanış <cihanntaniss@icloud.com>
2026-07-23 13:02:46 +0300

    Day 18:Change name with prompt

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ function toggleBackground(){
         isBlue=false;
     }
 }
+function changeName(){
+    let userName=prompt('What is your name?');
+    document.getElementById('myName').innerHTML=
+    'Hello'+userName+'!';
+}
