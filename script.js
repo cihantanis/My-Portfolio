904db255e507d1e3c3c08a754d461ac7e923e8c1
Cihan Tanış <cihanntaniss@icloud.com>
2026-07-28 12:47:56 +0300

    Day 23:Add visitor counter

--- script.js
+++ script.js
@@ -57,3 +57,10 @@ let today=new Date();
 document.getElementById('time').innerHTML=
 today;
 }
+let visitor=0;
+function countVisitor(){
+    visitors++;
+    document.getElementById('visitorCount').innerHTML=
+    'Visitor:'+visitors;
+}
+countVisitor();
